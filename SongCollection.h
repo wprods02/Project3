231a2071903b7e115804a2ca955e3b1efc3e5f74
zxjay08 <127056278+zxjay08@users.noreply.github.com>
2023-12-04 18:34:42 -0500

    Update SongCollection.h

--- SongCollection.h
+++ SongCollection.h
@@ -17,6 +17,5 @@ public:
 	void swap(int index1, int index2); // Swaps elements of Song Vector based on Input
 	void heapifyDown(int index,string attribute, int size); // Heap Sort Heapify Down
 	void heapSort(string attribute); // Heap Sort Method
-	int getNumSongs(); // Funtion that get the number of songs
 	void testPrint(string attribute); // Print the attributes of the Song including name and artist
 };
