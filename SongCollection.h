f421d8d8857ef2a8e46f10222d593cba04213177
zxjay08 <127056278+zxjay08@users.noreply.github.com>
2023-12-05 06:18:42 -0500

    Update SongCollection.h

--- SongCollection.h
+++ SongCollection.h
@@ -10,11 +10,6 @@ class SongCollection
 {
 public:
 	
-	void importSongs(); // Read in all files from CSV
-
-	void quickSort(string attribute, int low, int high); // Quick Sort 
-	int partition(string attribute, int low, int high); // Quick Sort Partition Method
-	void swap(int index1, int index2); // Swaps elements of Song Vector based on Input
 	void heapifyDown(int index,string attribute, int size); // Heap Sort Heapify Down
 	void heapSort(string attribute); // Heap Sort Method
 	void testPrint(string attribute); // Print the attributes of the Song including name and artist
