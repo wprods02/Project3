390a02ed829cffc918fd6c220925876d01faa486
zxjay08 <127056278+zxjay08@users.noreply.github.com>
2023-12-05 06:20:32 -0500

    Update SongCollection.h

--- SongCollection.h
+++ SongCollection.h
@@ -9,7 +9,8 @@
 class SongCollection
 {
 public:
-	
+	vector<Song> recommended1; // Vector that output recommended song
+	vector<Song> recommended2; // Vector that output recommended song, used for comparison of methods.
 	void heapifyDown(int index,string attribute, int size); // Heap Sort Heapify Down
 	void heapSort(string attribute); // Heap Sort Method
 	void testPrint(string attribute); // Print the attributes of the Song including name and artist
