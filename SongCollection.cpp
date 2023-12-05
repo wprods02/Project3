5ea5f1680b5d839ccccacf1d2dde456075c4d380
zxjay08 <127056278+zxjay08@users.noreply.github.com>
2023-12-05 06:19:41 -0500

    Update SongCollection.cpp

--- SongCollection.cpp
+++ SongCollection.cpp
@@ -2,57 +2,6 @@
 
 using namespace std;
 
-// Quick Sort
-void SongCollection::quickSort(string attribute, int low, int high)
-{
-
-	if (low < high) 
-	{
-		int piv = partition(attribute, low, high);
-
-		quickSort(attribute, low, piv - 1);
-		quickSort(attribute, piv + 1, high);
-	}
-}
-
-// Partition under Quick Sort
-int SongCollection::partition(string attribute, int low, int high)
-{
-
-	int up = low;
-	int down = high;
-
-	auto pivot = recommended2[low].getAttribute(attribute);
-	while (down > up) {
-		for (int i = high; i > low; i--) {
-			if (recommended2[down].getAttribute(attribute) < pivot)
-				break;
-			down--;
-		}
-		for (int i = up; i < high; i++) {
-			if (recommended2[up].getAttribute(attribute) > pivot)
-				break;
-			up++;
-		}
-
-		if (down > up)
-			swap(up, down);
-	}
-		
-	swap(low, down);
-	return down;
-	
-}
-
-// Swap index with user input
-void SongCollection::swap(int index1, int index2)
-{
-
-	auto temp = recommended2[index1];
-	recommended2[index1] = recommended2[index2];
-	recommended2[index2] = temp;
-}
-
 // Heapify Down
 void SongCollection::heapifyDown(int index, string attribute, int size)
 {
